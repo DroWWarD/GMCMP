293c5d207eafd9c9140b2113f8abc2e647a236f4
Дмитрий Чернов <109292707+DroWWarD@users.noreply.github.com>
2025-10-14 14:17:46 +0300

    SupportCall

--- composeApp/src/iosArm64Main/kotlin/ru/acs/grandmap/core/Platform.iosArm64.kt
+++ composeApp/src/iosArm64Main/kotlin/ru/acs/grandmap/core/Platform.iosArm64.kt
@@ -3,27 +3,17 @@ package ru.acs.grandmap.core
 
 import androidx.compose.runtime.Composable
 import platform.Foundation.NSURL
-import platform.Foundation.NSURLQueryAllowedCharacterSet
-import platform.Foundation.stringByAddingPercentEncodingWithAllowedCharacters
 import platform.UIKit.UIApplication
 
-actual class KmpContext // заглушка, контекст не нужен
+actual class KmpContext
 
 @Composable
 actual fun rememberKmpContext(): KmpContext = KmpContext()
 
 actual fun openDialer(ctx: KmpContext, phone: String) {
-    // iOS ожидает tel://<digits>; эскейпнем на всякий случай
-    val encoded = phone.stringByAddingPercentEncodingWithAllowedCharacters(
-        NSURLQueryAllowedCharacterSet
-    ) ?: phone
-    val url = NSURL(string = "tel://$encoded") ?: return
-
+    val url = NSURL(string = "tel://$phone") ?: return
     val app = UIApplication.sharedApplication
-    if (app.canOpenURL(url)) {
-        // Достаточно и этого вызова; на девайсе откроет dialer
-        app.openURL(url)
-        // Для iOS 10+ можно так:
-        // app.openURL(url, options = emptyMap<Any?, Any>(), completionHandler = null)
-    }
+    if (app.canOpenURL(url)) app.openURL(url)
 }
+
+actual fun isDialerSupported(): Boolean = true
